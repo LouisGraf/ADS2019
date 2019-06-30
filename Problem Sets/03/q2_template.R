3304ee604104ef16df1aae6ed8455e6af23bf01a
LouisGraf <mail@louis-graf.de>
2019-06-30 19:51:57 +0200

    30_06_1&2_Fast_Fertig

--- Problem Sets/03/q2_template.R
+++ Problem Sets/03/q2_template.R
@@ -4,6 +4,11 @@
 # Name: Louis Graf
 # Matrikelnummer: 2389931
 
+library(tidyverse) # tidy data analysis
+library(tidygraph) # tidy graph analysis
+library(ggraph) 
+library(jsonlite)
+
 
 if (!file.exists("clinton_emails.rda")) {
   clinton_emails <- fromJSON("http://graphics.wsj.com/hillary-clinton-email-documents/api/search.php?subject=&text=&to=&from=&start=&end=&sort=docDate&order=desc&docid=&limit=27159&offset=0")$rows
@@ -19,4 +24,32 @@ clinton_emails %>%
   filter(to != "") %>% 
   filter(!grepl(";", from)) %>% 
   filter(!grepl(";", to)) -> clinton_emails
+
+clinton_emails %>%
+  select(to, from) %>%
+  group_by(to, from) %>%
+  summarise(weight = n())%>%
+  arrange(desc(weight)) %>%
+  ungroup() -> clinton_route
+
+
+
+
+as_tbl_graph(clinton_route, directed = FALSE) %>%
+mutate(neighbors = centrality_degree(),
+       group = group_infomap(),
+       keyplayer = node_is_keyplayer(k = 10)) %>%
+  activate(edges) %>% 
+  filter(!edge_is_multiple()) -> clinton_network
+
+
+layout <- create_layout(clinton_network, 
+                        layout = "fr")
+
+ggraph(layout) + 
+  geom_edge_density(aes(fill = weight)) +
+  geom_edge_link(aes(width = weight), alpha = 0.1) + 
+  geom_node_point(aes(color = factor(group)), size = 2) +
+  geom_node_text(aes(label = name), size = 2, repel = TRUE) +
+  theme_graph()
 
