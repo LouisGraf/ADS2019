ce7c7b1bf3f4cdcd62c2d368ce862cbce6049fbe
LouisGraf <mail@louis-graf.de>
2019-07-15 23:57:04 +0200

    15_07_RandomForestProbleme

--- Problem Sets/04/q2_template.R
+++ Problem Sets/04/q2_template.R
@@ -8,6 +8,7 @@ require(tidyverse)
 library(foreign)
 library(tidytext)
 library(topicmodels)
+library(randomForest)
 
 #Working Directory anpassen, um einfacher an Ordnerstruktur zu gelangen
 setwd("Problem Sets/04/bbc")
@@ -40,7 +41,7 @@ book_data %>%
 book_freq %>%
   bind_tf_idf(word, category, n) -> idf_freq
 
-#Alltags-Wörter (zB: People) werden zwar häuig geschrieben, haben aber einen geringen Wert in der tf/idf-Bewertung
+#
 
 #c -----------------------
 #LDA
@@ -68,3 +69,56 @@ top_terms %>%
   facet_wrap(~ topic, scales = "free_x")
 
 #per-document classification
+book_split <- initial_split(book_freq, strata = "category", p = 0.80)
+book_train <- training(book_split)
+book_test <- testing(book_split)
+
+text_rec <- recipe(category ~ ., data = book_train) %>%
+  prep(training = book_train)
+
+book_train_data <- juice(text_rec)
+book_test_data  <- bake(text_rec, book_test)
+
+str(book_train_data, list.len = 10)
+
+
+
+#Random Forest
+# rf_model <- rand_forest(trees = 20,mode = "classification") %>%
+#   set_engine("randomForest")
+# rf_model
+# 
+# 
+# text_model <- rf_model %>%  #Error: cannot allocate vector of size 10.7 Gb LOL
+#   fit(category ~ ., data = book_train_data)
+
+train_preprocessed <- bake(text_rec, book_train_data)
+test_preprocessed <- bake(text_rec, book_test_data)
+
+
+text_model <- rand_forest(
+  mode = "classification",
+  trees = 250) %>%
+  set_engine("ranger") %>%
+  fit(category ~ ., data = book_train_data)
+
+predictions_rf <- text_model %>%
+  predict(new_data = test_preprocessed) %>%
+  bind_cols(test_preprocessed %>% dplyr::select(category))
+
+
+
+# eval_tibble = predict(text_model, book_test_data, type = "class")
+# eval_tibble$category = book_test_data$category
+# 
+# 
+# eval_tibble %>%
+#   accuracy(truth = category, estimate =  .pred_class )
+# 
+# 
+# eval_tibble %>%
+#   mutate(combined = paste(substr(label,1,10), substr(.pred_class,1,5), sep =" | ")) %>%
+#   group_by(combined) %>%
+#   tally() %>%
+#   arrange(-n) %>% 
+#   print(n=20)
